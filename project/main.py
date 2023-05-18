5b25abbc34622aab4ec5aa6d3eb1cdc6caf7bc54
William Hahn <william.hahn@siemens.com>
2023-05-17 19:19:14 -0500

    more table functions added

--- project/main.py
+++ project/main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_login import login_required, current_user
 from flask import Blueprint, render_template
+from .models import Schedule
 
 main = Blueprint('main', __name__)
 
@@ -14,6 +15,12 @@ def index():
 def profile():
     return render_template('profile.html')
 
+@main.route('/schedule')
+@login_required
+def schedule():
+    schedule = Schedule.query
+    return render_template('bootstrap_table.html', title='Bootstrap Table',
+                           schedule=schedule)
 
 # if __name__ == "__main__":
 #    app.run(host="127.0.0.1", port=8080, debug=True)
--- project/models.py
+++ project/models.py
@@ -7,3 +7,10 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+class Schedule(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    amount = db.Column(db.Numeric(10, 2))
+    frequency = db.Column(db.String(100))
+    startdate = db.Column(db.Date)
