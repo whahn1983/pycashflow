41812157c7f8c48b19c405cc16f0b97c9f9aae2a
William Hahn <william.hahn@siemens.com>
2023-05-28 11:36:30 -0500

    small sh fix

--- getemail.sh
+++ getemail.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
-DATABASE_URL=sqlite:///[DATABASE FOLDER LOCATION]
+export DATABASE_URL=sqlite:///[DATABASE FOLDER LOCATION]
 
 /usr/bin/python3 [LOCATION OF PYCASHFLOW PROJECT]/getemail.py
