425fe9c18e2c53f660ee967945f16b771e05bb31
William Hahn <william.hahn@siemens.com>
2023-06-21 14:48:06 -0500

    bug fix for emails with bad chars

--- app/getemail.py
+++ app/getemail.py
@@ -33,55 +33,58 @@ email_content = {}
 
 for i in range(1, messages + 1):
     # fetch the email message by ID
-    res, msg = imap.fetch(str(i), "(RFC822)")
-    for response in msg:
-        if isinstance(response, tuple):
-            # parse a bytes email into a message object
-            msg = email.message_from_bytes(response[1])
-            # decode the email subject
-            subject, encoding = decode_header(msg["Subject"])[0]
-            if isinstance(subject, bytes):
-                # if it's a bytes, decode to str
-                try:
-                    subject = subject.decode(encoding)
-                except:
-                    subject = "subject"
-            # decode email sender
-            From, encoding = decode_header(msg.get("From"))[0]
-            if isinstance(From, bytes):
-                try:
-                    From = From.decode(encoding)
-                except:
-                    pass
-            # if the email message is multipart
-            if msg.is_multipart():
-                # iterate over email parts
-                for part in msg.walk():
-                    # extract content type of email
-                    content_type = part.get_content_type()
-                    content_disposition = str(part.get("Content-Disposition"))
+    try:
+        res, msg = imap.fetch(str(i), "(RFC822)")
+        for response in msg:
+            if isinstance(response, tuple):
+                # parse a bytes email into a message object
+                msg = email.message_from_bytes(response[1])
+                # decode the email subject
+                subject, encoding = decode_header(msg["Subject"])[0]
+                if isinstance(subject, bytes):
+                    # if it's a bytes, decode to str
                     try:
-                        # get the email body
-                        body = part.get_payload(decode=True).decode()
+                        subject = subject.decode(encoding)
+                    except:
+                        subject = "subject"
+                # decode email sender
+                From, encoding = decode_header(msg.get("From"))[0]
+                if isinstance(From, bytes):
+                    try:
+                        From = From.decode(encoding)
                     except:
                         pass
-                    if content_type == "text/plain" and "attachment" not in content_disposition:
-                        # print text/plain emails and skip attachments
+                # if the email message is multipart
+                if msg.is_multipart():
+                    # iterate over email parts
+                    for part in msg.walk():
+                        # extract content type of email
+                        content_type = part.get_content_type()
+                        content_disposition = str(part.get("Content-Disposition"))
+                        try:
+                            # get the email body
+                            body = part.get_payload(decode=True).decode()
+                        except:
+                            pass
+                        if content_type == "text/plain" and "attachment" not in content_disposition:
+                            # print text/plain emails and skip attachments
+                            try:
+                                email_content[subject] = body
+                            except:
+                                pass
+                else:
+                    # extract content type of email
+                    content_type = msg.get_content_type()
+                    # get the email body
+                    body = msg.get_payload(decode=True).decode()
+                    if content_type == "text/plain":
+                        # print only text email parts
                         try:
                             email_content[subject] = body
                         except:
                             pass
-            else:
-                # extract content type of email
-                content_type = msg.get_content_type()
-                # get the email body
-                body = msg.get_payload(decode=True).decode()
-                if content_type == "text/plain":
-                    # print only text email parts
-                    try:
-                        email_content[subject] = body
-                    except:
-                        pass
+    except:
+        pass
 
 # formatted for Bank of America email alerts.  Re-format for your bank.
 try:
