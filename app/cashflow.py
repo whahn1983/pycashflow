eb85f4459dd95be9b0830f6a3189611fa888463d
William Hahn <william.hahn@siemens.com>
2023-06-29 08:42:32 -0500

    reorganized app with separate py for calculations and js external reference

---
+++ app/cashflow.py
@@ -0,0 +1,189 @@
+from app import db
+from .models import Schedule, Total, Running, Transactions
+from datetime import datetime
+import pandas as pd
+import json
+import plotly
+import plotly.express as px
+import os
+from dateutil.relativedelta import relativedelta
+from natsort import index_natsorted
+import numpy as np
+import decimal
+import plotly.graph_objs as go
+
+
+def calc_schedule(yearamount):
+    months = 12
+    weeks = 52
+    years = 1
+    quarters = 4
+    biweeks = 26
+
+    if yearamount == "1":
+        months = 12
+        weeks = 52
+        years = 1
+        quarters = 4
+        biweeks = 26
+    if yearamount == "2":
+        months = 24
+        weeks = 104
+        years = 2
+        quarters = 8
+        biweeks = 52
+    if yearamount == "3":
+        months = 36
+        weeks = 156
+        years = 3
+        quarters = 12
+        biweeks = 78
+    if yearamount == "4":
+        months = 48
+        weeks = 208
+        years = 4
+        quarters = 16
+        biweeks = 104
+
+    try:
+        engine = db.create_engine(os.environ.get('DATABASE_URL')).connect()
+    except:
+        engine = db.create_engine('sqlite:///db.sqlite').connect()
+
+    # pull the schedule information
+    df = pd.read_sql('SELECT * FROM schedule;', engine)
+
+    # loop through the schedule and create transactions in a table out to the future number of years
+    for i in range(len(df.index)):
+        format = '%Y-%m-%d'
+        name = df['name'][i]
+        startdate = df['startdate'][i]
+        frequency = df['frequency'][i]
+        amount = df['amount'][i]
+        type = df['type'][i]
+        if frequency == 'Monthly':
+            for k in range(months):
+                futuredate = datetime.strptime(startdate, format).date() + relativedelta(months=k)
+                if type == 'Income':
+                    rollbackdate = datetime.combine(futuredate, datetime.min.time())
+                    total = Total(type=type, name=name, amount=amount,
+                                  date=pd.tseries.offsets.BDay(1).rollback(rollbackdate).date())
+                else:
+                    total = Total(type=type, name=name, amount=amount, date=futuredate - pd.tseries.offsets.BDay(0))
+                db.session.add(total)
+        elif frequency == 'Weekly':
+            for k in range(weeks):
+                futuredate = datetime.strptime(startdate, format).date() + relativedelta(weeks=k)
+                total = Total(type=type, name=name, amount=amount, date=futuredate - pd.tseries.offsets.BDay(0))
+                db.session.add(total)
+        elif frequency == 'Yearly':
+            for k in range(years):
+                futuredate = datetime.strptime(startdate, format).date() + relativedelta(years=k)
+                total = Total(type=type, name=name, amount=amount, date=futuredate - pd.tseries.offsets.BDay(0))
+                db.session.add(total)
+        elif frequency == 'Quarterly':
+            for k in range(quarters):
+                futuredate = datetime.strptime(startdate, format).date() + relativedelta(months=3 * k)
+                total = Total(type=type, name=name, amount=amount, date=futuredate - pd.tseries.offsets.BDay(0))
+                db.session.add(total)
+        elif frequency == 'BiWeekly':
+            for k in range(biweeks):
+                futuredate = datetime.strptime(startdate, format).date() + relativedelta(weeks=2 * k)
+                total = Total(type=type, name=name, amount=amount, date=futuredate - pd.tseries.offsets.BDay(0))
+                db.session.add(total)
+        elif frequency == 'Onetime':
+            futuredate = datetime.strptime(startdate, format).date()
+            if futuredate < datetime.today().date():
+                onetimeschedule = Schedule.query.filter_by(name=name).first()
+                db.session.delete(onetimeschedule)
+            else:
+                total = Total(type=type, name=name, amount=amount, date=futuredate)
+                db.session.add(total)
+    db.session.commit()
+
+
+def calc_transactions(balance):
+    try:
+        engine = db.create_engine(os.environ.get('DATABASE_URL')).connect()
+    except:
+        engine = db.create_engine('sqlite:///db.sqlite').connect()
+
+    # retrieve the total future transactions
+    df = pd.read_sql('SELECT * FROM total;', engine)
+    df = df.sort_values(by="date", key=lambda x: np.argsort(index_natsorted(df["date"])))
+
+    # collect the next 60 days of transactions for the transactions table
+    format = '%Y-%m-%d'
+    for i in df.iterrows():
+        if datetime.today().date() + relativedelta(months=1) > \
+                datetime.strptime(i[1].date, format).date() > datetime.today().date():
+            transactions = Transactions(name=i[1].iloc[3], type=i[1].type, amount=i[1].amount,
+                                        date=datetime.strptime(i[1].date, format).date())
+            db.session.add(transactions)
+    db.session.commit()
+
+    # for schedules marked as expenses, make the value negative for the sum
+    for i in df.iterrows():
+        id = i[1].id
+        amount = i[1].amount
+        type = i[1].type
+        if type == 'Expense':
+            amount = float(amount) * -1
+            df.at[id - 1, 'amount'] = amount
+        elif type == 'Income':
+            pass
+
+    # group total transactions by date and sum the amounts for each date
+    df = df.groupby("date")['amount'].sum().reset_index()
+
+    # loop through the total transactions by date and add the sums to the total balance amount
+    runbalance = float(balance.amount)
+    running = Running(amount=runbalance, date=datetime.today().date())
+    db.session.add(running)
+    for i in df.iterrows():
+        format = '%Y-%m-%d'
+        rundate = i[1].date
+        amount = i[1].amount
+        if datetime.strptime(rundate, format).date() > datetime.today().date():
+            runbalance += amount
+            running = Running(amount=runbalance, date=datetime.strptime(rundate, format).date())
+            db.session.add(running)
+    db.session.commit()
+
+
+def plot_cash():
+    try:
+        engine = db.create_engine(os.environ.get('DATABASE_URL')).connect()
+    except:
+        engine = db.create_engine('sqlite:///db.sqlite').connect()
+
+    # plot the running balances by date on a line plot
+    df = pd.read_sql('SELECT * FROM running;', engine)
+    df = df.sort_values(by='date', ascending=False)
+    format = '%Y-%m-%d'
+    minbalance = df['amount'].min()
+    minbalance = decimal.Decimal(str(minbalance)).quantize(decimal.Decimal('.01'))
+    if float(minbalance) >= 0:
+        minrange = 0
+    else:
+        minrange = float(minbalance) * 1.1
+    maxbalance = 0
+    for i in df.iterrows():
+        if datetime.today().date() + relativedelta(months=2) > \
+                datetime.strptime(i[1].date, format).date() > datetime.today().date():
+            if i[1].amount > maxbalance:
+                maxbalance = i[1].amount
+    maxrange = maxbalance * 1.1
+    start_date = str(datetime.today().date())
+    end_date = str(datetime.today().date() + relativedelta(months=2))
+    layout = go.Layout(yaxis=dict(range=[minrange, maxrange]), xaxis=dict(range=[start_date, end_date]),
+                       margin=dict(l=5, r=20, t=35, b=5))
+    fig = px.line(df, x="date", y="amount", template="plotly", title="Cash Flow", line_shape="spline")
+    fig.update_layout(layout)
+    fig.update_xaxes(title_text='Date')
+    fig.update_yaxes(title_text='Amount')
+    fig.update_layout(paper_bgcolor="PaleTurquoise")
+
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return minbalance, graphJSON
--- app/main.py
+++ app/main.py
@@ -4,18 +4,10 @@ from flask import Blueprint, render_template
 from .models import Schedule, Balance, Total, Running, User, Settings, Transactions, Email
 from app import db
 from datetime import datetime
-import pandas as pd
-import json
-import plotly
-import plotly.express as px
 import os
 from sqlalchemy import desc, extract, asc
-from dateutil.relativedelta import relativedelta
-from natsort import index_natsorted
-import numpy as np
 from werkzeug.security import generate_password_hash
-import decimal
-import plotly.graph_objs as go
+from .cashflow import calc_schedule, calc_transactions, plot_cash
 
 
 main = Blueprint('main', __name__)
@@ -28,7 +20,6 @@ def index():
     balance = Balance.query.order_by(desc(Balance.date), desc(Balance.id)).first()
 
     try:
-        balance.amount
         if balance.amount:
             db.session.query(Balance).delete()
             balance = Balance(amount=balance.amount, date=datetime.today())
@@ -40,166 +31,23 @@ def index():
         db.session.add(balance)
         db.session.commit()
 
-    # retrieve the number of years for the cash flow plot
-    months = 12
-    weeks = 52
-    years = 1
-    quarters = 4
-    biweeks = 26
-    yearamount = request.form.get('yearamount')
-    if yearamount == "1":
-        months = 12
-        weeks = 52
-        years = 1
-        quarters = 4
-        biweeks = 26
-    if yearamount == "2":
-        months = 24
-        weeks = 104
-        years = 2
-        quarters = 8
-        biweeks = 52
-    if yearamount == "3":
-        months = 36
-        weeks = 156
-        years = 3
-        quarters = 12
-        biweeks = 78
-    if yearamount == "4":
-        months = 48
-        weeks = 208
-        years = 4
-        quarters = 16
-        biweeks = 104
-
     # empty the tables to create fresh data from the schedule
     db.session.query(Total).delete()
     db.session.query(Running).delete()
     db.session.query(Transactions).delete()
     db.session.commit()
-    try:
-        engine = db.create_engine(os.environ.get('DATABASE_URL')).connect()
-    except:
-        engine = db.create_engine('sqlite:///db.sqlite').connect()
 
-    # pull the schedule information
-    df = pd.read_sql('SELECT * FROM schedule;', engine)
+    # retrieve the number of years for the cash flow plot
+    yearamount = request.form.get('yearamount')
 
-    # loop through the schedule and create transactions in a table out to the future number of years
-    for i in range(len(df.index)):
-        format = '%Y-%m-%d'
-        name = df['name'][i]
-        startdate = df['startdate'][i]
-        frequency = df['frequency'][i]
-        amount = df['amount'][i]
-        type = df['type'][i]
-        if frequency == 'Monthly':
-            for k in range(months):
-                futuredate = datetime.strptime(startdate, format).date() + relativedelta(months=k)
-                if type == 'Income':
-                    rollbackdate = datetime.combine(futuredate, datetime.min.time())
-                    total = Total(type=type, name=name, amount=amount, date=pd.tseries.offsets.BDay(1).rollback(rollbackdate).date())
-                else:
-                    total = Total(type=type, name=name, amount=amount, date=futuredate - pd.tseries.offsets.BDay(0))
-                db.session.add(total)
-        elif frequency == 'Weekly':
-            for k in range(weeks):
-                futuredate = datetime.strptime(startdate, format).date() + relativedelta(weeks=k)
-                total = Total(type=type, name=name, amount=amount, date=futuredate - pd.tseries.offsets.BDay(0))
-                db.session.add(total)
-        elif frequency == 'Yearly':
-            for k in range(years):
-                futuredate = datetime.strptime(startdate, format).date() + relativedelta(years=k)
-                total = Total(type=type, name=name, amount=amount, date=futuredate - pd.tseries.offsets.BDay(0))
-                db.session.add(total)
-        elif frequency == 'Quarterly':
-            for k in range(quarters):
-                futuredate = datetime.strptime(startdate, format).date() + relativedelta(months=3 * k)
-                total = Total(type=type, name=name, amount=amount, date=futuredate - pd.tseries.offsets.BDay(0))
-                db.session.add(total)
-        elif frequency == 'BiWeekly':
-            for k in range(biweeks):
-                futuredate = datetime.strptime(startdate, format).date() + relativedelta(weeks=2 * k)
-                total = Total(type=type, name=name, amount=amount, date=futuredate - pd.tseries.offsets.BDay(0))
-                db.session.add(total)
-        elif frequency == 'Onetime':
-            futuredate = datetime.strptime(startdate, format).date()
-            if futuredate < datetime.today().date():
-                onetimeschedule = Schedule.query.filter_by(name=name).first()
-                db.session.delete(onetimeschedule)
-            else:
-                total = Total(type=type, name=name, amount=amount, date=futuredate)
-                db.session.add(total)
-    db.session.commit()
+    # calculate total events for the year amount
+    calc_schedule(yearamount)
 
-    # retrieve the total future transactions
-    df = pd.read_sql('SELECT * FROM total;', engine)
-    df = df.sort_values(by="date", key=lambda x: np.argsort(index_natsorted(df["date"])))
+    # calculate sum of running transactions
+    calc_transactions(balance)
 
-    # collect the next 60 days of transactions for the transactions table
-    format = '%Y-%m-%d'
-    for i in df.iterrows():
-        if datetime.today().date() + relativedelta(months=1) > \
-                datetime.strptime(i[1].date, format).date() > datetime.today().date():
-            transactions = Transactions(name=i[1].iloc[3], type=i[1].type, amount=i[1].amount, date=datetime.strptime(i[1].date, format).date())
-            db.session.add(transactions)
-    db.session.commit()
-
-    # for schedules marked as expenses, make the value negative for the sum
-    for i in df.iterrows():
-        id = i[1].id
-        amount = i[1].amount
-        type = i[1].type
-        if type == 'Expense':
-            amount = float(amount) * -1
-            df.at[id - 1, 'amount'] = amount
-        elif type == 'Income':
-            pass
-
-    # group total transactions by date and sum the amounts for each date
-    df = df.groupby("date")['amount'].sum().reset_index()
-
-    # loop through the total transactions by date and add the sums to the total balance amount
-    runbalance = float(balance.amount)
-    running = Running(amount=runbalance, date=datetime.today().date())
-    db.session.add(running)
-    for i in df.iterrows():
-        format = '%Y-%m-%d'
-        rundate = i[1].date
-        amount = i[1].amount
-        if datetime.strptime(rundate, format).date() > datetime.today().date():
-            runbalance += amount
-            running = Running(amount=runbalance, date=datetime.strptime(rundate, format).date())
-            db.session.add(running)
-    db.session.commit()
-
-    # plot the running balances by date on a line plot
-    df = pd.read_sql('SELECT * FROM running;', engine)
-    df = df.sort_values(by='date', ascending=False)
-    minbalance = df['amount'].min()
-    minbalance = decimal.Decimal(str(minbalance)).quantize(decimal.Decimal('.01'))
-    if minbalance >= 0:
-        minrange = 0
-    else:
-        minrange = float(minbalance) * 1.1
-    maxbalance = 0
-    for i in df.iterrows():
-        if datetime.today().date() + relativedelta(months=2) > \
-                datetime.strptime(i[1].date, format).date() > datetime.today().date():
-            if i[1].amount > maxbalance:
-                maxbalance = i[1].amount
-    maxrange = maxbalance * 1.1
-    start_date = str(datetime.today().date())
-    end_date = str(datetime.today().date() + relativedelta(months=2))
-    layout = go.Layout(yaxis=dict(range=[minrange, maxrange]), xaxis=dict(range=[start_date, end_date]),
-                       margin=dict(l=5, r=20, t=35, b=5))
-    fig = px.line(df, x="date", y="amount", template="plotly", title="Cash Flow", line_shape="spline")
-    fig.update_layout(layout)
-    fig.update_xaxes(title_text='Date')
-    fig.update_yaxes(title_text='Amount')
-    fig.update_layout(paper_bgcolor="PaleTurquoise")
-
-    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    # plot cash flow results
+    minbalance, graphJSON = plot_cash()
 
     return render_template('index.html', title='Index', balance=balance.amount, minbalance=minbalance,
                            graphJSON=graphJSON)
