a6ea2ff1960f740f229cafa2700847eb76223cc4
Bill Hahn <bill@hahn3.com>
2023-09-12 21:01:23 -0500

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # start cron
-#/usr/sbin/crond -f -l 8
+exec /usr/sbin/crond -f -l 8
 
 #flask migrate
-#/usr/local/bin/flask --app app db migrate
+exec /usr/local/bin/flask --app app db migrate
 
 #run waitress
 exec waitress-serve --listen=127.0.0.1:5000 --call app:create_app
