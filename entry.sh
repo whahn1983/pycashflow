c8b6542a06308438005f1150636e6dcf9db77a32
Bill Hahn <bill@hahn3.com>
2023-09-12 20:02:53 -0500

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-
+/bin/touch debug0
 # start cron
 /usr/sbin/crond -f -l 8
-
+/bin/touch debug1
 #flask migrate
 /usr/local/bin/flask --app app db migrate
 
-touch debug.test
+/bin/touch debug2
 #run waitress
 /usr/local/bin/waitress-serve --listen=127.0.0.1:5000 --call app:create_app &
 
-touch debug2.test2
+/bin/touch debug3
