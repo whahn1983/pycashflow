4cfbe514d64d1d59fa321de9141495f49e9dd83c
Bill Hahn <bill@hahn3.com>
2023-09-12 21:10:59 -0500

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -7,4 +7,4 @@
 /usr/local/bin/flask --app app db migrate > /dev/null 2>&1 & disown
 
 #run waitress
-exec waitress-serve --listen=127.0.0.1:5000 --call app:create_app > /dev/null 2>&1 & disown
+exec waitress-serve --listen=127.0.0.1:5000 --call app:create_app
