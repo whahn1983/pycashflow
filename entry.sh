dcda9dac53ad83e2dcd5ad991ffd608249af2012
Bill Hahn <bill@hahn3.com>
2023-10-16 21:25:03 -0500

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -4,7 +4,9 @@
 /usr/sbin/crond -f -l 8 > /dev/null 2>&1 &
 
 #flask migrate
+/usr/local/bin/flask --app app db init > /dev/null 2>&1
 /usr/local/bin/flask --app app db migrate > /dev/null 2>&1
+/usr/local/bin/flask --app app db upgrade > /dev/null 2>&1
 
 #run waitress
 exec waitress-serve --listen=0.0.0.0:5000 --call app:create_app
