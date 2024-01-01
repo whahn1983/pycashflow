8e059e200b5eec311f823fd40de4bab4dbd8c09d
William Hahn <william.hahn@siemens.com>
2024-01-01 15:05:08 -0600

    cleaned up some html and added logging for flask migrate.

--- entry.sh
+++ entry.sh
@@ -4,9 +4,9 @@
 /usr/sbin/crond -f -l 8 > /dev/null 2>&1 &
 
 #flask migrate
-/usr/local/bin/flask --app app db init > /dev/null 2>&1
-/usr/local/bin/flask --app app db migrate > /dev/null 2>&1
-/usr/local/bin/flask --app app db upgrade > /dev/null 2>&1
+/usr/local/bin/flask --app app db init
+/usr/local/bin/flask --app app db migrate
+/usr/local/bin/flask --app app db upgrade
 
 #run waitress
 exec waitress-serve --listen=0.0.0.0:5000 --call app:create_app
