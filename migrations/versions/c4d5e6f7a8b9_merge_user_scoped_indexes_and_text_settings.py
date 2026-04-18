4c45bac4a654761f4720c849242fef7f17fd9454
Bill Hahn <bill@hahn3.com>
2026-04-17 22:02:10 -0500

    Apply sibling indexes in merge head migration

--- migrations/versions/c4d5e6f7a8b9_merge_user_scoped_indexes_and_text_settings.py
+++ migrations/versions/c4d5e6f7a8b9_merge_user_scoped_indexes_and_text_settings.py
@@ -6,6 +6,10 @@ Create Date: 2026-04-18 00:00:00.000000
 
 """
 
+from alembic import op
+
+import sqlalchemy as sa
+
 
 # revision identifiers, used by Alembic.
 revision = 'c4d5e6f7a8b9'
@@ -15,7 +19,31 @@ depends_on = None
 
 
 def upgrade():
-    pass
+    bind = op.get_bind()
+    inspector = sa.inspect(bind)
+
+    indexes_by_table = {
+        table_name: {index['name'] for index in inspector.get_indexes(table_name)}
+        for table_name in ('balance', 'hold', 'skip', 'email')
+        if inspector.has_table(table_name)
+    }
+
+    if 'ix_balance_user_id' not in indexes_by_table.get('balance', set()):
+        op.create_index(op.f('ix_balance_user_id'), 'balance', ['user_id'], unique=False)
+
+    if 'ix_balance_user_id_date_id' not in indexes_by_table.get('balance', set()):
+        op.create_index(
+            'ix_balance_user_id_date_id', 'balance', ['user_id', 'date', 'id'], unique=False
+        )
+
+    if 'ix_hold_user_id' not in indexes_by_table.get('hold', set()):
+        op.create_index(op.f('ix_hold_user_id'), 'hold', ['user_id'], unique=False)
+
+    if 'ix_skip_user_id' not in indexes_by_table.get('skip', set()):
+        op.create_index(op.f('ix_skip_user_id'), 'skip', ['user_id'], unique=False)
+
+    if 'ix_email_user_id' not in indexes_by_table.get('email', set()):
+        op.create_index(op.f('ix_email_user_id'), 'email', ['user_id'], unique=False)
 
 
 def downgrade():
