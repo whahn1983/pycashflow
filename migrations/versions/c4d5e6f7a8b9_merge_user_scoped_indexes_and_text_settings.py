18ee5dadae2fd9ca157b8615106a3832b0fa6dc8
Bill Hahn <bill@hahn3.com>
2026-04-17 21:53:58 -0500

    Preserve migration ancestry with merge revision

---
+++ migrations/versions/c4d5e6f7a8b9_merge_user_scoped_indexes_and_text_settings.py
@@ -0,0 +1,22 @@
+"""merge user-scoped indexes and text settings heads
+
+Revision ID: c4d5e6f7a8b9
+Revises: b1c2d3e4f5a6, a8c9d0e1f2a3
+Create Date: 2026-04-18 00:00:00.000000
+
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = 'c4d5e6f7a8b9'
+down_revision = ('b1c2d3e4f5a6', 'a8c9d0e1f2a3')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
